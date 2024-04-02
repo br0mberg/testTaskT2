4d6ab502c9aacd23e587895d1af2565f75600133
Andrey Brombin <arcadka@gmail.com>
2024-04-02 19:38:00 +0300

    Initial commit

---
+++ src/main/java/com/brombin/Tele2task/Tele2taskApplication.java
@@ -0,0 +1,13 @@
+package com.brombin.Tele2task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tele2taskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tele2taskApplication.class, args);
+	}
+
+}
